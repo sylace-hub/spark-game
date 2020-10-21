72841eb97371cda688cf8645fc4cc30acce0c488
kostarter <ko_starter@hotmail.com>
2020-10-21 20:04:19 +0200

    gestion des notebooks zeppelin

--- download-and-unzip.sh
+++ download-and-unzip.sh
@@ -11,6 +11,19 @@ if [ "$1" == "--help" ]; then
     exit 0
 fi
 
+# Download data archive for the s3 bucket
 bucketname="${1,,}-eur"
 aws s3 cp s3://"$bucketname"/work-dir.zip .
-unzip work-dir.zip && rm work-dir.zip
+unzip work-dir.zip
+
+if [ -d "./notebook" ]; then
+    if [ ! -d "/mnt/var/lib/zeppelin/notebook" ]; then
+	echo "Info : Zeppelin notebooks does not exist, create it."
+        sudo mkdir -p /mnt/var/lib/zeppelin/notebook
+    fi
+    sudo cp -r ./notebook/* /mnt/var/lib/zeppelin/notebook
+    # TODO : y a surement un chown pour les notebooks copiés mais je sais pas a qui ? 
+fi
+
+# Remove temporary files
+rm -rf work-dir.zip ./notebook
--- zip-and-upload.sh
+++ zip-and-upload.sh
@@ -11,6 +11,17 @@ if [ "$1" == "--help" ]; then
     exit 0
 fi
 
+# Check if Zeppelin notebooks are available
+if [ -d "/mnt/var/lib/zeppelin/notebook" ]; then
+    cp -r /mnt/var/lib/zeppelin/notebook .
+else 
+    echo "Warnig: Zeppelin notebooks directory does not exist."
+fi
+
+# Create the archive and upload it to the bucket
 zip -r work-dir.zip * -x "*.sh"
 bucketname="${1,,}-eur"
 aws s3 cp work-dir.zip s3://"$bucketname"
+
+# Remove temporary files
+rm -rf notebook/ work-dir.zip
